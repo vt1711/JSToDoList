cd421700e69bf7433601e2f73fe190010a12b07c
V7777 <vedantt7777@gmail.com>
2021-06-03 21:53:20 +0530

    Completed basic todo list with html,css and js

---
+++ index.js
@@ -0,0 +1,46 @@
+var txtnd, opdiv, str = "", lii, ctxtnd, cbtnn ;
+
+var ip = document.querySelector("#ip");
+var adbtn = document.querySelector("#addbtn");
+adbtn.addEventListener("click", addtxt);
+
+function del(e) {
+    e.parentNode.remove();
+}
+
+function addtxt() {
+    str = ip.value;
+    if (str == "") {
+        alert("Please Enter list item");
+        return false;
+    }
+    else {
+        //creating list
+        lii = document.createElement("li");
+        lii.setAttribute("class", "liitem");
+        txtnd = document.createTextNode(str);
+        lii.appendChild(txtnd);
+
+        //creating cancel button
+        cbtnn = document.createElement("button");
+        cbtnn.setAttribute("class", "cbtn");
+        ctxtnd = document.createTextNode("x");
+        cbtnn.appendChild(ctxtnd);
+        cbtnn.onclick = function (event) {
+            del(event.currentTarget);
+        }
+
+        //adding button to list
+        lii.appendChild(cbtnn);
+
+        //adding list to output div
+        opdiv = document.querySelector("#op");
+        opdiv.appendChild(lii);
+
+
+        ip.value = "";
+        ip.focus();
+    }
+}
+
+// adbtn.style.backgroundColor="grey";
